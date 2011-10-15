22b7fc158d69cebc45474deb2f4bc446805187a9
Matias H. Leidemer <matiasleidemer@gmail.com>
2011-10-15 01:42:35 -0300

    removed trash on item

--- lib/pagseguro_catcher/transaction/item.rb
+++ lib/pagseguro_catcher/transaction/item.rb
@@ -7,18 +7,6 @@ module PagseguroCatcher
         self.body = body[:sender]
       end
       
-      def name
-        self[:name].utf8
-      end
-      
-      def email
-        self[:email]
-      end
-      
-      def phone
-        { :area => self[:phone][:areaCode], :number => self[:phone][:number] }
-      end
-
     end
     
   end
